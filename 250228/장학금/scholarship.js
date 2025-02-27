d583a1e267e55c06746bb12b7f0cabf868f12a0e
Il-Woo-Choi <chldlfdn0440@naver.com>
2025-02-28 02:42:20 +0900

    Title: 장학금, Time: 164ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250228/장학금/scholarship.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let mid = Number(input[0]);
+let fin = Number(input[1]);
+
+if(mid >= 90){
+    if(fin >= 95){
+        console.log(100000);
+    } else if(fin >= 90){
+        console.log(50000);
+    }
+} else{
+    console.log(0);
+}
