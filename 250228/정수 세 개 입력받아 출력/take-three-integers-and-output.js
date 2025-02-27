380f95f435666aa18168805b5cf7ff26f896463a
Il-Woo-Choi <chldlfdn0440@naver.com>
2025-02-28 00:14:55 +0900

    Title: 정수 세 개 입력받아 출력, Time: 166ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250228/정수 세 개 입력받아 출력/take-three-integers-and-output.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().split("\n");
+
+let input1 = input[0].split(" ");
+let a = Number(input1[0]);
+let b = Number(input1[1]);
+let c = Number(input[1]);
+
+console.log(a, b, c)
