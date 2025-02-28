5b5c248041e83328f6838acf725930b266a554c0
Il-Woo-Choi <chldlfdn0440@naver.com>
2025-02-28 21:49:15 +0900

    Title: 문자 출력하기, Time: 173ms, Memory: 15MB, Status: Passed - CodeTree

--- 250228/문자 출력하기/print-text.js
+++ 250228/문자 출력하기/print-text.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim();
 
-let N = Number(input);
+let N = input;
 let result = "";
 
 for(i = 0; i < 8; i++){
-    result += "G";
+    result += N;
 }
 
 console.log(result);
