9f45848b3701459dcb7e450bcb673b2c53a0c745
Il-Woo-Choi <chldlfdn0440@naver.com>
2025-02-28 19:01:13 +0900

    Title: 일 수 구하기, Time: 185ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250228/일 수 구하기/number-of-days-in-month.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+
+let n = Number(input);
+
+if(n % 2 == 1){
+    if(n < 8){
+        console.log("31");
+    } else{
+        console.log("30");
+    }
+} else if(n % 2 == 0){
+    if(n < 8){
+        if(n == 2){
+            console.log("28");
+        } else{
+            console.log("31");
+        }
+    } else{
+        console.log("30");
+    }
+}
