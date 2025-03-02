1bded553c9f21c03123b86c6ac537e0105b97dc3
Il-Woo-Choi <chldlfdn0440@naver.com>
2025-03-02 18:19:42 +0900

    Title: 369 게임, Time: 172ms, Memory: 15MB, Status: Passed - CodeTree

--- 250302/369 게임/369-game.js
+++ 250302/369 게임/369-game.js
@@ -1,30 +1,19 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().trim();
+let n = Number(fs.readFileSync(0).toString().trim());
 
-let N = Number(input);
 let result = "";
-
-for(i=1; i<=N; i++){
-    if(parseInt(i/10) == 0 && i <= 9){
-        if(i % 3 == 0){
-            result += 0 + " ";
-        } else{
-            result += i + " ";
-        }
-    } else if((parseInt(i/10)) % 3 == 1){
-        if(i % 3 == 1 && i != 10){
-            result += 0 + " ";
-        } else {
-            result += i + " ";
-        }
-    } else if((parseInt(i/10)) % 3 == 2){
-        if(i % 3 == 2){
-            result += 0 + " ";
-        } else {
-            result += i + " ";
-        }
-    } else if((parseInt(i/10)) % 3 == 0 && i>9){
-        result += 0 + " ";
+for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0) {
+        result += "0 ";
+    }
+    else if (i % 10 === 3 || i % 10 === 6 || i % 10 === 9) {
+        result += "0 ";
+    }
+    else if (parseInt(i / 10) === 3 || parseInt(i / 10) === 6 || parseInt(i / 10) === 9) {
+        result += "0 ";
+    }
+    else {
+        result += i + " ";
     }
 }
 
