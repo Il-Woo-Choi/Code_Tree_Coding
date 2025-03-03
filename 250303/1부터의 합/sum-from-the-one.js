e7f3063433bceafda443372a0bc82cf8b5d95d5a
Il-Woo-Choi <chldlfdn0440@naver.com>
2025-03-03 17:58:25 +0900

    Title: 1부터의 합, Time: 177ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250303/1부터의 합/sum-from-the-one.js
+++ 250303/1부터의 합/sum-from-the-one.js
@@ -5,11 +5,11 @@ let N = Number(input);
 let sum = 0;
 
 for(let i = 1; i <= 100; i++){
+    sum += i;
     if(sum >= N){
-        sum -= i-1;
+        sum -= i;
         break;
     }
-    sum += i;
 }
 
 console.log(sum);
