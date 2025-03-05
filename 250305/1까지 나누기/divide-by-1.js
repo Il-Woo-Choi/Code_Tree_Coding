3afeaf8eaa18a9165933ba507296aa817dab1d15
Il-Woo-Choi <chldlfdn0440@naver.com>
2025-03-05 17:32:01 +0900

    Title: 1까지 나누기, Time: 175ms, Memory: 15MB, Status: Passed - CodeTree

--- 250305/1까지 나누기/divide-by-1.js
+++ 250305/1까지 나누기/divide-by-1.js
@@ -2,13 +2,13 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim();
 
 let N = Number(input);
-let i = 1;
 let count = 0;
+let i = 1;
 
-while(N/i > 1){
-    N /= i;
+while (N > 1) {
+    N = Math.floor(N / i);
     count++;
     i++;
 }
 
-console.log(count+1);
+console.log(count);
