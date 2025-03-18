82da8d52a0df530a6b0a2b4af5e32bbcbe355573
Il-Woo-Choi <chldlfdn0440@naver.com>
2025-03-18 12:10:08 +0900

    Title: 열 단위로 반복하여 출력, Time: 176ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250318/열 단위로 반복하여 출력/print-in-column.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+
+let n = Number(input);
+let str = "";
+
+for(let i = 0; i < n; i++){
+    str = "";
+    for(let j = 0; j < n; j++){
+        str += i+1;
+    }
+
+    console.log(str);
+}
