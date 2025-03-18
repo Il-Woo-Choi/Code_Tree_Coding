b36b4effaad0c097bef8aaa7d376b900a867babf
Il-Woo-Choi <chldlfdn0440@naver.com>
2025-03-18 12:20:20 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 174ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250318/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+
+let N = Number(input);
+let str = "";
+
+for(let i = N; i > 0; i--){
+    str = "";
+    for(let j = N; j > 0; j--){
+        str += j + " ";
+    }
+    console.log(str);
+}
