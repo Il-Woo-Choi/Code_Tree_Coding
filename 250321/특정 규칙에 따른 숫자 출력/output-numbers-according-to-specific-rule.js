31198e331797416563335b6de798ee96fa5ba19e
Il-Woo-Choi <chldlfdn0440@naver.com>
2025-03-21 14:10:35 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 218ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250321/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+
+let N = Number(input);
+let str = "";
+let cnt = 1;
+
+for(let i = 0; i < N; i++){
+    str = "";
+    for(let j = 0; j < i; j++){
+        str += "  ";
+    }
+
+    for(let j = 0; j < N - i; j++){
+        str += cnt + " ";
+        cnt++;
+    }
+
+    console.log(str);
+
+    if(cnt >= 9){
+        cnt = 1;
+    }
+}
