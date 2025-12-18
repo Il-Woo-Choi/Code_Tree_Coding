a93a3c31baf919061e9192ab4613146ec44bf43f
IlWooChoi <ilwoo.choi.00@gmail.com>
2025-12-18 13:03:18 +0900

    Title: 배열에서 원소의 특징, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251218/배열에서 원소의 특징/characteristics-of-elements-in-an-array.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+let arr = [];
+
+for(let i = 0; i < input.length; i++){
+    if(input[i] % 3 === 0){
+        break;
+    } else {
+        arr.push(input[i]);
+    }
+}
+
+console.log(arr[arr.length-1]);
