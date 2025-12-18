7bd5248bc2a48b46e8e9ae2672222c55bb18d5c3
IlWooChoi <ilwoo.choi.00@gmail.com>
2025-12-18 14:38:39 +0900

    Title: 연속부분수열일까, Time: 62ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251218/연속부분수열일까/contiguous-array-or-not.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let N = input[0].split(" ").map(Number);
+let A = input[1].split(" ").map(Number);
+let B = input[2].split(" ").map(Number);
+let count = 0;
+
+for(let i = 0; i < A.length; i++){
+    if(A[i] === B[0]){
+        for(let j = 0; j < B.length; j++){
+            if(A[i+j] === B[j]){
+                count++;
+            }
+        }
+        break;
+    }
+}
+
+if(count === B.length){
+    console.log("Yes");
+} else {
+    console.log("No");
+}
