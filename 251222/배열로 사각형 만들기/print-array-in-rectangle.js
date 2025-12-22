f997f531e4bd6462d1481c19706c86e74ac34c02
IlWooChoi <ilwoo.choi.00@gmail.com>
2025-12-22 13:17:43 +0900

    Title: 배열로 사각형 만들기, Time: 58ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251222/배열로 사각형 만들기/print-array-in-rectangle.js
@@ -0,0 +1,20 @@
+let arr2d = Array(5).fill(0).map(() => Array(5).fill(0));
+
+for(let i = 0; i < 5; i++){
+    arr2d[0][i] = 1;
+    arr2d[i][0] = 1;
+}
+
+for(let i = 1; i < 5; i++){
+    for(let j = 1; j < 5; j++){
+        arr2d[i][j] = arr2d[i-1][j] + arr2d[i][j-1];
+    }
+}
+
+for(let i = 0; i < 5; i++){
+    let str = "";
+    for(let j = 0; j < 5; j++){
+        str += arr2d[i][j] + " ";
+    }
+    console.log(str);
+}
