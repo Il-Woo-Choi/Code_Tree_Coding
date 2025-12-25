32934ca77ce5797ca9bb2dbc82d393160453f717
IlWooChoi <ilwoo.choi.00@gmail.com>
2025-12-25 21:41:12 +0900

    Title: 사칙연산 함수, Time: 63ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 251225/사칙연산 함수/quadratic-operations-function.js
@@ -0,0 +1,34 @@
+const add = (a, b) => {
+    return a+b;
+}
+
+const minus = (a, b) => {
+    return a-b;
+}
+
+const multi = (a, b) => {
+    return a*b;
+}
+
+const divide = (a, b) => {
+    return parsInt(a/b);
+}
+
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let o = input[1];
+let b = Number(input[2]);
+
+if(o == "+"){
+    console.log(`${a} + ${b} = ${add(a, b)}`);
+} else if(o == "-"){
+    console.log(`${a} - ${b} = ${minus(a, b)}`);
+} else if(o == "*"){
+    console.log(`${a} * ${b} = ${multi(a, b)}`);
+} else if(o == "/"){
+    console.log(`${a} / ${b} = ${divide(a, b)}`);
+} else {
+    console.log("False");
+}
