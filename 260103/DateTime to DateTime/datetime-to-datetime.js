a02491768415a004ebcb3711596a83b02aa7a66f
IlWooChoi <ilwoo.choi.00@gmail.com>
2026-01-03 14:22:47 +0900

    Title: DateTime to DateTime, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

--- 260103/DateTime to DateTime/datetime-to-datetime.js
+++ 260103/DateTime to DateTime/datetime-to-datetime.js
@@ -3,7 +3,7 @@ let input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
 let [a, b, c] = input;
 
-if(a < 11 ||  a >= 11 && b < 11 || a >= 11 && b >= 11 && c < 11){
+if(a < 11 ||  a == 11 && b < 11 || a == 11 && b == 11 && c < 11){
     console.log(-1);
 } else {
     console.log((a-11)*60*24 + (b-11)*60 + (c-11));
