c81402ee30ec685fc7665480dd489ce4b5f8c7ff
IlWooChoi <ilwoo.choi.00@gmail.com>
2026-01-03 14:20:57 +0900

    Title: DateTime to DateTime, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260103/DateTime to DateTime/datetime-to-datetime.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+let [a, b, c] = input;
+
+if(a < 11 ||  a >= 11 && b < 11 || a >= 11 && b >= 11 && c < 11){
+    console.log(-1);
+} else {
+    console.log((a-11)*60*24 + (b-11)*60 + (c-11));
+}
